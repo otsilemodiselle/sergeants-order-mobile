26a574e3cc2b25f09fb581a8ee5c5d92f58990a6
Otsile Modiselle <otsilemodiselle@outlook.com>
2025-03-20 23:10:22 +0200

    install important start packages

--- sergeants_orders_app/App.tsx
+++ sergeants_orders_app/App.tsx
@@ -1,29 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, ImageBackground, TextInput, Text }from 'react-native';;
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.paragraph}>
-        Hello World!
-      </Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+ 
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: 'black',
-    padding: 8,
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: "white",
-  },
-});
