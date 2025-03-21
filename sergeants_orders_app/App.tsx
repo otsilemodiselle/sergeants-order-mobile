835fb7f4401a8f534fd0888c81c00808a073db9b
Otsile Modiselle <otsilemodiselle@outlook.com>
2025-03-21 08:54:46 +0200

    setup Axios backend

--- sergeants_orders_app/App.tsx
+++ sergeants_orders_app/App.tsx
@@ -1,7 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, ImageBackground, TextInput, Text }from 'react-native';;
-
+import { Button, Text, View, StyleSheet }from 'react-native';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 export default function App() {
- 
+
+  const endpointURL = "https://67dc7d76e00db03c406839c3.mockapi.io/food"
+
+  const getListOfFood = async() => {
+    
+    try{
+      console.log("Button Pressed: Fetching food list...");
+      const response = await axios.get(endpointURL)
+      console.log(JSON.stringify(response.data, null, 3));
+    } 
+    catch(error)
+    {
+      console.error("Error fetching food list:", error)
+    }}
+
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+      <StatusBar style="auto"/>
+      <Button 
+        title="Get List of Food" 
+        onPress={getListOfFood}
+      />
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+})
